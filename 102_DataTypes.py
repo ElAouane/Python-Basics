b4626f4f3dc389f810feeaa4c8bef298c22a9556
Hamza El Aouane <haouane@spartaglobal.com>
2020-02-04 15:28:26 +0000

    new methods

--- 102_DataTypes.py
+++ 102_DataTypes.py
@@ -15,3 +15,31 @@ name = 'Hamza'
 welcome_message = 'Welcome to the DANGERZOOOOOOONEEEEEEE!'
 
 print("{}, {}".format(name, welcome_message))
+
+#Special methods for strings
+my_string_new = " HeLLoooO this is an amazing string"
+
+#.count()
+print(my_string_new.count(''))
+
+#.strip() removes trailing white spaces
+print(my_string_new)
+print(my_string_new.strip())
+
+#Len() - function not a method()
+print(len(my_string_new))
+print(len(my_string_new.strip()))
+
+#Capitalize()
+print(my_string_new.strip())
+print(my_string_new.strip().capitalize())
+
+#EVERY thing upper
+print(my_string_new.upper())
+
+#Everything lower
+print(my_string_new.lower())
+
+#a method to separate the string into several string
+new_string = my_string_new.split()
+print(new_string)
